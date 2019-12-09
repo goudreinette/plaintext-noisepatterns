5f05a194be37a705a065fdb4f76a8899ab197f3f
Rein van der Woerd <reinvanderwoerd@me.com>
2019-12-09 20:01:32 +0100

    Attempt at replicating the effect in pure JS, but it's not fast enough...

---
+++ main.js
@@ -0,0 +1,40 @@
+import {set_text, real_text} from './Cargo.toml'
+let {millis} = p5.prototype
+
+
+const newsAPIKey = 'cadf03152b7a4215896a1082e2025109'
+
+
+
+/**
+ * Fetch a news article and give it to Rust for initialization
+ */
+fetch(`https://newsapi.org/v2/top-headlines?country=nl&apiKey=cadf03152b7a4215896a1082e2025109`).then(response => {
+    response.json().then(({articles}) => {
+        let aggregatedArticles = ""
+        for (const article of shuffleArray(articles.slice(0, 3))) {
+            aggregatedArticles += ` ${article.title} ------ `
+        }
+
+        set_text(aggregatedArticles)
+
+        new p5();
+        let $portrait = document.querySelector('#portrait')
+        setInterval(() => {
+            $portrait.innerHTML = real_text(innerWidth, innerHeight, millis())
+        }, 16)
+    })
+})
+
+
+
+/**
+ * Utils
+ */
+function shuffleArray(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
